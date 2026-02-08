e3135e02ac000292f54724df6a78ba3a4f115fd4
Radmila44 <radmila.kelecevic@gmail.com>
2026-02-08 22:33:19 +0100

    Izmijene u main.py na main grani

--- main.py
+++ main.py
@@ -7,16 +7,14 @@ print("\nFinal Project - Rad sa GitHubom i Pythonom\n")
 manager = ProductManager()
 
 # # Dodavanje proizvoda
-p1 = Product("Laptop", 1200.00, 5)
+p1 = Product("Mobilni", 1200.00, 5)
 p2 = Product("Slušalice", 150.00, 10)
-p3 = Product("Monitor", 500, 10)
+p3 = Product("Monitor", 500, 20)
+p4 = Product("Tastatura", 200.00, 20)
+p5 = Product("Miš", 30.00, 150)
 
 manager.add_product(p1)
 manager.add_product(p2)
 manager.add_product(p3)
-
-# Prikaz svih proizvoda
-manager.display_all_products()
-
-# Ukupna vrijednost inventara
-manager.total_value()
+manager.add_product(p4)
+manager.add_product(p5)
